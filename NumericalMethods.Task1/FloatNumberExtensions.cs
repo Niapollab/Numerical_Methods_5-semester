1b4ab834b99fd7114ab694a21b59c086b7c52d45
Niapoll <kolya21082001@gmail.com>
2021-09-26 20:44:45 +0300

    Added three phases in first task

---
+++ NumericalMethods.Task1/FloatNumberExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumericalMethods.Task1
+{
+    public static class FloatNumberExtensions
+    {
+        public static int CompareTo(this double first, double second, double eps)
+        {
+            return Math.Abs(first - second) <= eps
+                ? 0
+                : (first - second > eps ? 1 : -1);
+        }
+
+        public static int CompareTo(this float first, float second, float eps)
+        {
+            return Math.Abs(first - second) <= eps
+                ? 0
+                : (first - second > eps ? 1 : -1);
+        }
+
+        public static int CompareTo(this decimal first, decimal second, decimal eps)
+        {
+            return Math.Abs(first - second) <= eps
+                ? 0
+                : (first - second > eps ? 1 : -1);
+        }
+    }
+}
--- NumericalMethods.Task1/Program.cs
+++ NumericalMethods.Task1/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using NumericalMethods.Core;
-using NumericalMethods.Core.Extensions;
-using NumericalMethods.Core.Interfaces;
+using System.Text;
 
 namespace NumericalMethods.Task1
 {
@@ -22,33 +19,236 @@ namespace NumericalMethods.Task1
     /// 					*	*				*	*
     ///
     ///                     d   e                 a     b     c
+    /// f - Right side
     /// </example>
-    public class FirstTaskMatrix : LeakyVectorMatrix<double>
+    public class FirstTaskMatrix
     {
+        private readonly int _rowsCount;
+
+        private readonly int _columnsCount;
+
+        private readonly double[] _a;
+
+        private readonly double[] _b;
+
+        private readonly double[] _c;
+
+        private readonly double[] _d;
+
+        private readonly double[] _e;
+
+        private readonly double[] _f;
+
         public FirstTaskMatrix(double[,] matrix)
-            : base(matrix.GetLength(0), matrix.GetLength(1), BuildLeakyMatrixVectors(matrix))
         {
+            _rowsCount = matrix.GetLength(0);
+            _columnsCount = matrix.GetLength(1);
+
+            _a = new double[_rowsCount - 1];
+            for (int i = 0; i + 1 < _rowsCount && i < _columnsCount; ++i)
+                _a[i] = matrix[i + 1, i];
+
+            _b = new double[_rowsCount];
+            for (int i = 0; i < _rowsCount && i < _columnsCount; ++i)
+                _b[i] = matrix[i, i];
+
+            _c = new double[_columnsCount - 1];
+            for (int i = 0; i < _rowsCount && i + 1 < _columnsCount; ++i)
+                _c[i] = matrix[i, i + 1];
+
+            _d = new double[_rowsCount];
+            for (int i = 0; i < _rowsCount && 5 < _columnsCount; ++i)
+                _d[i] = matrix[i, 5];
+
+            _e = new double[_rowsCount];
+            for (int i = 0; i < _rowsCount && 6 < _columnsCount; ++i)
+                _e[i] = matrix[i, 6];
+
+            _f = new double[_rowsCount];
+            for (int i = 0; i < _rowsCount; ++i)
+                _f[i] = matrix[i, _columnsCount - 1];
+
+            FirstPhase();
+            SecondPhase();
+            ThirdPhase();
         }
 
-        private static IEnumerable<ILeakyMatrixVector<double>> BuildLeakyMatrixVectors(double[,] matrix)
+        private void ThirdPhase()
         {
-            return new ILeakyMatrixVector<double>[] 
+            for (int i = 0; i < _rowsCount; ++i)
             {
-                new UnderTheMainDiagonal<double>(matrix),
-                new MainDiagonalVector<double>(matrix),
-                new UpperTheMainDiagonal<double>(matrix),
-                new ColumnVector<double>(matrix, 5),
-                new ColumnVector<double>(matrix, 6)
-            };
+                if (i != 5 && _d[i] != 0)
+                {
+                    DevideLine(i, _d[i]);
+                    
+                    _d[i] -= _d[5];
+                    _e[i] -= _e[5];
+                }
+            }
+            //_a[5] = _d[6];
+            //_a[6] = _e[7];
+
+            //_b[6] = _e[6];
+
+            //_c[4] = _d[4];
+            //_c[4] = _e[5];
         }
+
+        private void MultiplyLine(int rowIndex, double element)
+        {
+            if (IsBelongsToA(rowIndex))
+                _a[rowIndex - 1] *= element;
+
+            _b[rowIndex] *= element;
+            _d[rowIndex] *= element;
+            _e[rowIndex] *= element;
+            _f[rowIndex] *= element;
+
+            if (IsBelongsToC(rowIndex))
+                _c[rowIndex] *= element;
+        }
+
+        private void DevideLine(int rowIndex, double element)
+        {
+            if (IsBelongsToA(rowIndex))
+                _a[rowIndex - 1] /= element;
+
+            _b[rowIndex] /= element;
+            _d[rowIndex] /= element;
+            _e[rowIndex] /= element;
+            _f[rowIndex] /= element;
+
+            if (IsBelongsToC(rowIndex))
+                _c[rowIndex] /= element;
+        }
+
+        private void SubCurrentFromNext(int rowIndex)
+        {
+            if (rowIndex < _rowsCount - 1)
+            {
+                _a[rowIndex] -= _b[rowIndex];
+                _b[rowIndex + 1] -= _c[rowIndex];
+                _d[rowIndex + 1] -= _d[rowIndex];
+                _e[rowIndex + 1] -= _e[rowIndex];
+                _f[rowIndex + 1] -= _f[rowIndex];
+            }
+            else
+                throw new ArgumentOutOfRangeException(nameof(rowIndex), "");
+        }
+
+        private void SubPrevFromCurrent(int rowIndex)
+        {
+            if (rowIndex > 0)
+            {
+                _c[rowIndex - 1] -= _b[rowIndex];
+                _b[rowIndex - 1] -= _a[rowIndex - 1];
+                _d[rowIndex] -= _d[rowIndex - 1];
+                _e[rowIndex] -= _e[rowIndex - 1];
+                _f[rowIndex] -= _f[rowIndex - 1];
+            }
+            else
+                throw new ArgumentOutOfRangeException(nameof(rowIndex), "");
+        }
+
+        private void FirstPhase()
+        {
+            for (int i = 0; i <= 5; ++i)
+            {
+                if (_b[i] == 0)
+                    continue;
+
+                DevideLine(i, _b[i]);
+
+                if (_a[i] == 0)
+                    continue;
+
+                DevideLine(i + 1, _a[i]);
+
+                SubCurrentFromNext(i);
+            }
+        }
+
+        private void SecondPhase()
+        {
+            for (int i = _rowsCount - 1; i > 6; --i)
+            {
+                if (_b[i] == 0)
+                    continue;
+
+                DevideLine(i, _b[i]);
+
+                if (_c[i - 1] == 0)
+                    continue;
+
+                DevideLine(i - 1, _c[i - 1]);
+
+                SubPrevFromCurrent(i);
+            }
+        }
+
+        private bool IsBelongsToC(int rowIndex)
+            => rowIndex < _rowsCount - 1;
+
+        private static bool IsBelongsToA(int rowIndex)
+            => rowIndex > 0;
+
+        private string ToString(int digitsAfterComma, char separator = '\t')
+        {
+            var builder = new StringBuilder();
+            var leakyMatrix = new Dictionary<(int, int), double>();
+
+            for (int i = 0; i + 1 < _rowsCount && i < _columnsCount; ++i)
+                if (!leakyMatrix.ContainsKey((i + 1, i)))
+                    leakyMatrix.Add((i + 1, i), _a[i]);
+
+            for (int i = 0; i < _rowsCount && i < _columnsCount; ++i)
+                if (!leakyMatrix.ContainsKey((i, i)))
+                    leakyMatrix.Add((i, i), _b[i]);
+
+            for (int i = 0; i < _rowsCount && i + 1 < _columnsCount; ++i)
+                if (!leakyMatrix.ContainsKey((i, i + 1)))
+                    leakyMatrix.Add((i, i + 1), _c[i]);
+
+            for (int i = 0; i < _rowsCount && 5 < _columnsCount; ++i)
+                if (!leakyMatrix.ContainsKey((i, 5)))
+                    leakyMatrix.Add((i, 5), _d[i]);
+
+            for (int i = 0; i < _rowsCount && 6 < _columnsCount; ++i)
+                if (!leakyMatrix.ContainsKey((i, 6)))
+                    leakyMatrix.Add((i, 6), _e[i]);
+
+            for (int i = 0; i < _rowsCount; ++i)
+                if (!leakyMatrix.ContainsKey((i, _columnsCount - 1)))
+                    leakyMatrix.Add((i, _columnsCount - 1), _f[i]);
+
+            for (int i = 0; i < _rowsCount; ++i)
+            {
+                for (int j = 0; j < _columnsCount; ++j)
+                {
+                    if (leakyMatrix.ContainsKey((i, j)))
+                        builder.Append(Math.Round(leakyMatrix[(i, j)], digitsAfterComma));
+                    else
+                        builder.Append('0');
+                    builder.Append(separator);
+                }
+                builder.Remove(builder.Length - 1, 1);
+                builder.Append(Environment.NewLine);
+            }
+            builder.Remove(builder.Length - Environment.NewLine.Length, Environment.NewLine.Length);
+
+            return builder.ToString();
+        }
+
+        public override string ToString()
+            => ToString(2);
     }
 
     class Program
     {
-        static ILeakyMatrix<double> GenerateMatrix()
+        static double[,] GenerateMatrix()
         {
             var random = new Random();
-            var matrix = new double[12,12];
+            var matrix = new double[12, 12];
             for (var i = 0; i < matrix.GetLength(0); ++i)
             {
                 matrix[i, i] = random.Next(2, 10);
@@ -58,90 +258,36 @@ namespace NumericalMethods.Task1
 
                 if (i + 1 < matrix.GetLength(0))
                     matrix[i + 1, i] = random.Next(2, 10);
-                
+
                 matrix[i, 5] = random.Next(2, 10);
 
                 matrix[i, 6] = random.Next(2, 10);
 
             }
-            return new FirstTaskMatrix(matrix);
-        }
-
-        static ILeakyMatrix<double> ReadMatrixFromConsole(char separator = ' ')
-        {
-            var lines = ConsoleExtensions.EnumerateLines();
-            double[][] matrix = lines.Select(line =>
-            {
-                return line
-                    .Split(separator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                    .Select(el => double.Parse(el))
-                    .ToArray();
-            }).ToArray();
-            
-            int minRowLength = matrix.Select(row => row.Length).Min();
-
-            var result = new double[matrix.Length, minRowLength];
-
-            for (int rowIndex = 0; rowIndex < result.GetLength(0); ++rowIndex)
-                for (int columnIndex = 0; columnIndex < result.GetLength(1); ++columnIndex)
-                    result[rowIndex, columnIndex] = matrix[rowIndex][columnIndex];
-
-            return new FirstTaskMatrix(result);
+            return matrix;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите матрицу: ");
-            ILeakyMatrix<double> matrix = ReadMatrixFromConsole();
+            var rawMatrix = new double[,]
+            {
+                {-9, -6, 0, 0, 0, -4, -7, 0, 0, 0, -37},
+                {-5, 2, 10, 0, 0, 1, -2, 0, 0, 0, 5},
+                {0, -7, -7, -2, 0, -7, -6, 0, 0, 0, -42},
+                {0, 0, 6, -1, -1, -1, -2, 0, 0, 0, -2},
+                {0, 0, 0, -4, 1, 1, 0, 0, 0, 0, -1},
+                {0, 0, 0, 0, 9, -7, 8, 0, 0, 0, 11},
+                {0, 0, 0, 0, 0, 5, 0, -8, 0, 0, -6},
+                {0, 0, 0, 0, 0, 10, 2, 3, 8, 0, 46},
+                {0, 0, 0, 0, 0, -2, 6, 8, 6, 8, 52},
+                {0, 0, 0, 0, 0, 6, -1, 0, -6, -8, -18}
+            };
+            // var rawMatrix = GenerateMatrix();
+            var matrix = new FirstTaskMatrix(rawMatrix);
 
-            ReduceAdditionalDiagonale(matrix);
-            RemoveBadArray(matrix, 5);
-            RemoveBadArray(matrix, 6);
-            SimplifyMainDiagonale(matrix);
+            Console.WriteLine(matrix);
 
-            Console.WriteLine(matrix.ToMatrixString());
             Console.ReadKey(true);
-        }
-
-        private static void SimplifyMainDiagonale(ILeakyMatrix<double> matrix)
-        {
-            for (int rowIndex = 0; rowIndex < matrix.RowsCount; ++rowIndex)
-                matrix.SafeDivideBy(rowIndex, matrix[rowIndex, rowIndex]);
-        }
-
-        private static void ReduceAdditionalDiagonale(ILeakyMatrix<double> matrix)
-        {
-            for (int rowIndex = 0; rowIndex < matrix.RowsCount / 2; ++rowIndex)
-            {
-                int nextRow = rowIndex + 1;
-                matrix
-                    .SafeDivideBy(rowIndex, matrix[rowIndex, rowIndex])
-                    .SafeDivideBy(nextRow, matrix[nextRow, rowIndex])
-                    .SubLines(nextRow, rowIndex);
-            }
-
-            for (int rowIndex = matrix.RowsCount - 1; rowIndex > matrix.RowsCount / 2; --rowIndex)
-            {
-                int prevRow = rowIndex - 1;
-                matrix
-                    .SafeDivideBy(rowIndex, matrix[rowIndex, rowIndex])
-                    .SafeDivideBy(prevRow, matrix[prevRow, rowIndex])
-                    .SubLines(prevRow, rowIndex);
-            }
-        }
-
-        private static void RemoveBadArray(ILeakyMatrix<double> matrix, int badIndex)
-        {
-            matrix.SafeDivideBy(matrix.RowsCount / 2, matrix[matrix.RowsCount / 2, matrix.RowsCount / 2]);
-
-            for (int rowIndex = 0; rowIndex < matrix.RowsCount; ++rowIndex)
-            {
-                if (rowIndex == badIndex)
-                    continue;
-                matrix
-                    .SafeDivideBy(rowIndex, matrix[rowIndex, badIndex])
-                    .SubLines(rowIndex, badIndex);
-            }
         }
     }
 }
