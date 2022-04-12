4ce535c0c539e6e7aaee91c8a427d2dbae45d50e
Niapoll <kolya21082001@gmail.com>
2022-04-12 10:18:15 +0300

    Add skeleton for task 7

---
+++ NumericalMethods.Task7/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NumericalMethods.Core.Collections;
+using NumericalMethods.Task5.Interpolating;
+using NumericalMethods.Task5.Models;
+using NumericalMethods.Task5.Splitting;
+using NumericalMethods.Core.Extensions;
+using System.Threading.Tasks;
+using NumericalMethods.UI;
+using System.Drawing;
+using NumericalMethods.Task5.Readers;
+
+namespace NumericalMethods.Task6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadKey(true);
+        }
+    }
+}
