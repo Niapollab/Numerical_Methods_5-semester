5640d3c054e329a18ee54bc39eddbf3f772b4204
Niapoll <kolya21082001@gmail.com>
2022-06-04 14:19:44 +0300

    Add Task 8

---
+++ NumericalMethods.Task8/BaseRungeSolver.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using NumericalMethods.Task8.Exceptions;
+
+namespace NumericalMethods.Task8
+{
+    abstract class BaseRungeSolver
+    {
+        protected const double MaxSegmentsCount = 64e6;
+
+        protected readonly double _y0;
+
+        protected readonly double _z0;
+
+        protected readonly Func<(double X, double Y, double Z), double> _yDerivative;
+
+        protected readonly Func<(double X, double Y, double Z), double> _zDerivative;
+
+        public BaseRungeSolver(double y0, double z0, Func<(double X, double Y, double Z), double> yDerivative, Func<(double X, double Y, double Z), double> zDerivative)
+        {
+            _y0 = y0;
+            _z0 = z0;
+            _yDerivative = yDerivative ?? throw new ArgumentNullException(nameof(yDerivative));
+            _zDerivative = zDerivative ?? throw new ArgumentNullException(nameof(zDerivative));
+        }
+
+        public IEnumerable<IReadOnlyList<(double X, double Y, double Z)>> EnumerateSolutions(double segmentStart, double segmentEnd, int segmentsCount, double eps)
+        {
+            if (segmentStart > segmentEnd)
+                (segmentStart, segmentEnd) = (segmentEnd, segmentStart);
+
+            if (segmentsCount > MaxSegmentsCount)
+                throw new RungeSolverException(0, "The number of segments has reached its maximum value. Step too small.");
+
+            (double X, double Y, double Z) startPoint = (segmentStart, _y0, _z0);
+
+            double step = GetSegmentDeltaStep(segmentStart, segmentEnd, segmentsCount);
+            IReadOnlyList<(double X, double Y, double Z)> currentSolution = FindSolutions(startPoint, segmentsCount, step);
+            yield return currentSolution;
+
+            double prevZError = 0;
+            for (var i = 1; true; ++i)
+            {
+                segmentsCount *= 2;
+
+                if (segmentsCount > MaxSegmentsCount)
+                    throw new RungeSolverException(i, "The number of segments has reached its maximum value. Step too small.");
+
+                step = GetSegmentDeltaStep(segmentStart, segmentEnd, segmentsCount);
+                IReadOnlyList<(double X, double Y, double Z)> nextSolution = FindSolutions(startPoint, segmentsCount, step);
+
+                double currentZError = CalculateError(currentSolution[^1].Z, nextSolution[^1].Z);
+                double currentYError = CalculateError(currentSolution[^1].Y, nextSolution[^1].Y);
+
+                if (Math.Abs(currentZError) - Math.Abs(prevZError) == 0)
+                    throw new RungeSolverException(i, "Reducing the step does not affect the size of the error.");
+
+                yield return nextSolution;
+
+                if (currentZError <= eps && currentYError <= eps)
+                    yield break;
+
+                prevZError = currentZError;
+                currentSolution = nextSolution;
+            }
+        }
+
+        protected abstract IReadOnlyList<(double X, double Y, double Z)> FindSolutions((double X, double Y, double Z) startPoint, int segmentsCount, double step);
+
+        protected abstract double CalculateError(double prevValue, double currentValue);
+
+        protected static double GetSegmentDeltaStep(double segmentStart, double segmentEnd, int segmentsCount)
+            => (segmentEnd - segmentStart) / segmentsCount;
+
+        protected static double GetDotByIndex(double startValue, double index, double step)
+            => startValue + index * step;
+    }
+}
---
+++ NumericalMethods.Task8/RungeSolver.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace NumericalMethods.Task8
+{
+    class RungeSolver : BaseRungeSolver
+    {
+        public const int Power = 4;
+
+        public RungeSolver(double y0, double z0, Func<(double X, double Y, double Z), double> yDerivative, Func<(double X, double Y, double Z), double> zDerivative) : base(y0, z0, yDerivative, zDerivative)
+        {
+        }
+
+        protected override IReadOnlyList<(double X, double Y, double Z)> FindSolutions((double X, double Y, double Z) point, int segmentsCount, double step)
+        {
+            int pointsCount = segmentsCount + 1;
+            List<(double X, double Y, double Z)> solutions = new List<(double X, double Y, double Z)>();
+
+            solutions.Add((point.X, point.Y, point.Z));
+
+            for (var i = 1; i < pointsCount; i++)
+            {
+                (double X, double Y, double Z) prevSolution = solutions[^1];
+
+                double x = GetDotByIndex(point.X, i - 1, step);
+
+                double k1 = step * _yDerivative((x, prevSolution.Y, prevSolution.Z));
+                double l1 = step * _zDerivative((x, prevSolution.Y, prevSolution.Z));
+
+                double k2 = step * _yDerivative((x + step / 2, prevSolution.Y + k1 / 2, prevSolution.Z + l1 / 2));
+                double l2 = step * _zDerivative((x + step / 2, prevSolution.Y + k1 / 2, prevSolution.Z + l1 / 2));
+
+                double k3 = step * _yDerivative((x + step / 2, prevSolution.Y + k2 / 2, prevSolution.Z + l2 / 2));
+                double l3 = step * _zDerivative((x + step / 2, prevSolution.Y + k2 / 2, prevSolution.Z + l2 / 2));
+
+                double k4 = step * _yDerivative((x + step, prevSolution.Y + k3, prevSolution.Z + l3));
+                double l4 = step * _zDerivative((x + step, prevSolution.Y + k3, prevSolution.Z + l3));
+
+                double y = prevSolution.Y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+                double z = prevSolution.Z + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
+
+                solutions.Add((x, y, z));
+            }
+
+            return solutions;
+        }
+
+        protected override double CalculateError(double prevValue, double currentValue)
+            => Math.Abs(prevValue - currentValue) / (Math.Pow(2, Power) - 1);
+    }
+}
